bb4507273e683ae9021ff63a424b2a93678f0ed6
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-12-19 08:15:30 +0100

    comment out

--- routes/stripe.js
+++ routes/stripe.js
@@ -10,7 +10,7 @@ router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     billing_address_collection: 'auto',
     line_items: [{
-        price: req.body.price_id, // Pass the price ID from the form
+        price: req.body.price_id,
         quantity: 1,
       }],
     mode: 'subscription',
@@ -22,13 +22,8 @@ router.post('/create-checkout-session', async (req, res) => {
 });
 
 router.post('/create-portal-session', async (req, res) => {
-  // For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
-  // Typically this is stored alongside the authenticated user in your database.
   const { session_id } = req.body;
   const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
-
-  // This is the url to which the customer will be redirected when they are done
-  // managing their billing with the portal.
   const returnUrl = YOUR_DOMAIN;
 
   const portalSession = await stripe.billingPortal.sessions.create({
